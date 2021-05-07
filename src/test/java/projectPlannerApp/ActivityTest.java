284ffdfb1f7e507af0d861145b2b7920ff041c1c
Collinnn <51782585+Collinnn@users.noreply.github.com>
2021-05-07 18:51:56 +0200

    Test cases

---
+++ src/test/java/projectPlannerApp/ActivityTest.java
@@ -0,0 +1,14 @@
+package projectPlannerApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ActivityTest {
+
+	@Test
+	void testA() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/projectPlannerApp/EmployeeTest.java
@@ -0,0 +1,14 @@
+package projectPlannerApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+
+	@Test
+	void testA() {
+		
+	}
+
+}
